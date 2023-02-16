4a17cdae163a8c65fdbfe870f145456c51c461f9
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2023-02-15 21:46:06 -0300

    fix contexto countdown

--- src/components/pages/Home/Countdown/index.tsx
+++ src/components/pages/Home/Countdown/index.tsx
@@ -4,7 +4,8 @@ import { CyclesContext } from '..'
 import { CountdownContainer, Separator } from './styles'
 
 export function Countdown() {
-  const { activeCycle, activeCycleId } = useContext(CyclesContext)
+  const { activeCycle, activeCycleId, markCurrentCycleAsFinished } =
+    useContext(CyclesContext)
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
@@ -17,7 +18,6 @@ export function Countdown() {
           new Date(),
           activeCycle.startDate,
         )
-
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished()
           setAmountSecondsPassed(totalSeconds)
